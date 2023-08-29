c2a9f929fa91230e3ef15053bb1ffeb7307bf1aa
Carlos Andrés Sánchez <57685118+dev2619@users.noreply.github.com>
2023-08-28 23:11:18 -0500

    Project progress day #2 - 28/08-2023
    - Prisma install
    - Prisma Schema, controller and service example based on Nestjs docs (https://docs.nestjs.com/recipes/prisma).
    - .env Configurated with databaase on planet scale.
    - Adjust in prettier error : 'end of line'.
    - Integrate Swagger docs based on documentation (https://docs.nestjs.com/openapi/introduction#setup-options), partially.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Lulu Api')
+    .setDescription('The lulu API is here ⚡')
+    .setVersion('1.0')
+    .addTag('lulu')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  console.log(`app on top ⚡ http://localhost:${port}`);
+
+  await app.listen(port);
 }
 bootstrap();
