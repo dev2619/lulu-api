c35c6add59d284765d940e5468cd032897b2e0ce
Carlos Andrés Sánchez <57685118+dev2619@users.noreply.github.com>
2023-08-31 22:08:06 -0500

    Project progress day #5 - 31/08-2023
    - Implement get user

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { User as UserModel } from '@prisma/client';
+import { Prisma, User as UserModel } from '@prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
@@ -15,5 +22,18 @@ export class UserController {
     @Body(new ValidationPipe()) userData: CreateUserDto,
   ): Promise<UserModel> {
     return this.userService.createUser(userData);
+  }
+
+  /**
+   * Obtain an specific user
+   */
+  @Get('/:id')
+  async getUser(
+    @Param('id', new ValidationPipe()) id: string,
+  ): Promise<UserModel> {
+    const userWhereUniqueInput: Prisma.UserWhereUniqueInput = {
+      id: Number(id), // Assuming 'id' is the unique identifier field
+    };
+    return this.userService.user(userWhereUniqueInput);
   }
 }
