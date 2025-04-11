78e7b54596a6558e7ea6dd7e9c59bb481ff475cc
Carlos Andrés Sánchez <caso2619dev@gmail.com>
2025-04-10 21:59:24 -0500

    initial new

---
+++ src/infrastructure/controllers/app.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller'; // Path relative to new spec file location
+import { AppService } from '../../application/app.service'; // Updated path
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
